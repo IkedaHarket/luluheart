0088fc25bdc4d021cd39ec9b2fa353b0890f3dd6
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-26 21:54:46 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'click';
+ @ViewChild('btn') button!: ElementRef<HTMLButtonElement>;
+
+  label:string = "¿Me quieres?"
+  love: boolean = false;
+  move(){
+    this.button.nativeElement.style.top = 
+    this.randomNumber(document.documentElement.clientHeight) +"px";
+    this.button.nativeElement.style.left = 
+    this.randomNumber(document.documentElement.clientWidth) +"px";
+
+  }
+  randomNumber(max:number) {
+    return Math.round(Math.random() * ((max - 100) - 100) + 100);
+  }
+
+  wiii(){
+    this.label = "La quiero mucho ;3"
+    this.love = true;
+  }
+  jum(){
+    this.label = "Yo si la quiero <3"
+  }
 }
